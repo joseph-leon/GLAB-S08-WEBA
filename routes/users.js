4ca994fe64066796aa6e5167fe52665b1bbe4e6b
joseph-leon <joseph.leon@tecsup.edu.pe>
2023-05-17 00:32:10 -0500

    terminado

---
+++ routes/users.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  res.render('index', { users });
+});
+
+router.post('/', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // hashea la contraseña :D
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  await newUser.save();
+  res.redirect('/users');
+});
+
+router.get('/edit/:id', async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.render('partials/edit', { user });
+});
+
+router.post('/update/:id', async (req, res) => {
+  await User.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect('/users');
+});
+
+router.get('/delete/:id', async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.redirect('/users');
+});
+
+module.exports = router;
